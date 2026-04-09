fae48c6881ddcfde2c7c455ff0e2e87557c0ffdc
Aviranjan <ad3447578@gmail.com>
2026-04-09 10:26:41 +0530

    Create a Post System

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ function handleSubmit(event) {
     message.style.color = "green"
   }
 }
+
+function addPost() {
+  let input = document.getElementById("postInput").value;
+  let container = document.getElementById("postContainer");
+
+  if(input === "") {
+    alert("Write Something First ❌");
+    return;
+  }
+
+  let postDiv = document.createElement("div");
+
+  let newPost = document.createElement("p");
+  newPost.innerText = input;
+
+  let deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "Delete";
+
+  deleteBtn.onclick = function() {
+    postDiv.remove();
+  }
+
+  postDiv.appendChild(newPost);
+  postDiv.appendChild(deleteBtn);
+
+  container.appendChild(postDiv);
+  Document.getElementById("postInput").value = "";
+
+}
